ca0f8cd0b3cf02a0044946d4ef2a0f6567c27697
diop <fodesdiop@gmail.com>
2018-01-29 16:48:28 -0800

    Test lottery contract single, or multiple players

--- compile.js
+++ compile.js
@@ -2,9 +2,9 @@ const path = require('path')
 const fs = require('fs')
 const solc = require('solc')
 
-const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol')
-const source = fs.readFileSync(inboxPath, 'utf8')
+const lotteryPath = path.resolve(__dirname, 'contracts', 'Lottery.sol')
+const source = fs.readFileSync(lotteryPath, 'utf8')
 
-let contract = solc.compile(source, 1).contracts[':Inbox']
+let contract = solc.compile(source, 1).contracts[':Lottery']
 
 module.exports = contract
